a70fad18e297aa35e82f2af53449b184190556cf
Team Glitch <keegan.bruer@gmail.com>
2020-02-11 18:22:59 -0500

    Climber should work now!

--- src/main/java/frc/robot/Climber.java
+++ src/main/java/frc/robot/Climber.java
@@ -1,12 +1,33 @@
 package frc.robot;
+
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 public class Climber{
-    TalonSRX cMotor;
-    public Climber(int c) {
+    TalonSRX cMotor, cMotor1;
+    DoubleSolenoid solenoid1, solenoid2;
+    Boolean extended, exdown;
+    public Climber(int c, int c1, int s, int s1) {
         cMotor = new TalonSRX(c);
+        cMotor1 = new TalonSRX(c1);
+        cMotor1.follow(cMotor);
+        solenoid1 = new DoubleSolenoid(s, s1);
     }
     public void climb(){
-        
+        if (extended && !exdown){
+            solenoid1.set(DoubleSolenoid.Value.kReverse);
+            extended=!extended;
+            exdown = true;
+        }
+        else if(!exdown){
+            solenoid1.set(DoubleSolenoid.Value.kForward);
+            extended=!extended;
+            exdown = true;
+        }
+        else {
+            exdown=false;
+        }   
     }
 }
