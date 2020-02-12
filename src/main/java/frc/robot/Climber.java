a707327e658b398a98e71164dbee4c18a081aa36
Team Glitch <keegan.bruer@gmail.com>
2020-02-11 19:08:25 -0500

    General Update

--- src/main/java/frc/robot/Climber.java
+++ src/main/java/frc/robot/Climber.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
@@ -9,25 +10,34 @@ public class Climber{
     TalonSRX cMotor, cMotor1;
     DoubleSolenoid solenoid1, solenoid2;
     Boolean extended, exdown;
-    public Climber(int c, int c1, int s, int s1) {
+    DigitalInput uplimit, downlimit;
+    public Climber(int c, int c1, int s, int s1, int up, int down) {
         cMotor = new TalonSRX(c);
         cMotor1 = new TalonSRX(c1);
         cMotor1.follow(cMotor);
         solenoid1 = new DoubleSolenoid(s, s1);
+        uplimit = new DigitalInput(up);
+        downlimit = new DigitalInput(down);
     }
+    
     public void climb(){
         if (extended && !exdown){
             solenoid1.set(DoubleSolenoid.Value.kReverse);
+            cMotor.set(ControlMode.PercentOutput, -0.5);
             extended=!extended;
             exdown = true;
         }
         else if(!exdown){
-            solenoid1.set(DoubleSolenoid.Value.kForward);
+            solenoid1.set(DoubleSolenoid.Value.kReverse);
+            cMotor.set(ControlMode.PercentOutput, -0.5);
             extended=!extended;
             exdown = true;
         }
         else {
             exdown=false;
-        }   
+        } 
+        if (!downlimit.get() && extended){
+
+        }  
     }
 }
--- src/main/java/frc/robot/Handeler.java
+++ src/main/java/frc/robot/Handeler.java
@@ -45,9 +45,6 @@ public class Handeler{
             extend.set(DoubleSolenoid.Value.kReverse);
         }
         
-
-
-        
         if (shoot){
 
         }
