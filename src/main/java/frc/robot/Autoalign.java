880ee3f1ecdef89e53861b16892b93c2b7bcd0f4
yoyobobface <bzcamp443@gmail.com>
2020-02-29 10:22:08 -0500

    Auto align code

--- src/main/java/frc/robot/Autoalign.java
+++ src/main/java/frc/robot/Autoalign.java
@@ -1,7 +1,43 @@
 package frc.robot;
 
 public class Autoalign extends Robot{
-    public static void alignShot(){
+    private static boolean aligning;
+    private static boolean alignedAngle = false;
+    private static boolean alignedDistance = false;
+    private static double turnpTerm = 1;
+    private static double turniTerm = 1;
+    private static double turndTerm = 1;
+    private static double turnPID;
+    private static double robotAngle;
+    private static double robotDistance;
+    private static double movepTerm = 1;
+    private static double moveiTerm = 1;
+    private static double movedTerm = 1;
+    private static double movePID;
+    private static double acum;
+    public static void alignShot(boolean input){
+        aligning = 
+        if(aligning){
+            if(!alignedAngle){
+                robotAngle = Math.atan((/*right lidar subtract left lidear divided by distance between them*/));\
+             if(robotAngle <= 2 && robotAngle >= -2){
+                 alignedAngle = true;
+             }
+             else{
+                 acum += robotAngle;
+                    turnPID = (robotAngle*turnpTerm)*(acum*turniTerm)*(Math.pow(robotAngle,2)*turndTerm);
+                drivetrain.arcade(0,turnPID);
+                }
+            }
+            else if(alignedDistance){
+                robotDistance = /*average of both lidar*/;
+                acum += robotDistance;
+                movePID = (robotDistance*movepTerm)*(acum*moveiTerm)*(Math.pow(robotDistance,2)*movedTerm);
+                drivetrain.aracde(movePID,0);
+            }
+        }
+        else{
 
+        }
     }
 }
