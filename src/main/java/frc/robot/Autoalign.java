a78a5b3fe62fa077b20c5e092cd238ce52ea6c45
yoyobobface <bzcamp443@gmail.com>
2020-02-29 12:53:43 -0500

    Re-wrote auto align code

--- src/main/java/frc/robot/Autoalign.java
+++ src/main/java/frc/robot/Autoalign.java
@@ -4,6 +4,7 @@ public class Autoalign extends Robot{
     private boolean activeAlign;
     private boolean alignedAngle = false;
     private boolean alignedDistance = false;
+    private double aimAngle;
     private double turnpTerm = 1;
     private double turniTerm = 1;
     private double turndTerm = 1;
@@ -19,29 +20,42 @@ public class Autoalign extends Robot{
     private double Lsep = 1;
     private double Usof = 1;
     private double Gdis = 1;
-    public void alignShot(boolean input){
-        if(input){activeAlign = !activeAlign;if(activeAlign){alignedAngle = false;alignedDistance = false;}}
-        if(activeAlign){
-            if(!alignedAngle){
-                robotAngle = Math.atan((Lfof-Lfof)/Lsep);
-             if(robotAngle <= 2 && robotAngle >= -2){
-                 alignedAngle = true;
-             }
-             else{
-                 acum += robotAngle;
-                    turnPID = (robotAngle*turnpTerm)*(acum*turniTerm)*(Math.pow(robotAngle,2)*turndTerm);
-                drivetrain.arcade(0,turnPID);
-                }
+    private boolean firstRun = true;
+    public int alignShot(boolean input){
+        if(input){//checks state change
+            activeAlign = !activeAlign;
+            if(activeAlign){
+                firstRun = true;
             }
-            else if(alignedDistance){
-                robotDistance = ((Lfof-Lfof)/Lsep);
-                acum += robotDistance;
-                movePID = (robotDistance*movepTerm)*(acum*moveiTerm)*(Math.pow(robotDistance,2)*movedTerm);
-                drivetrain.arcade(movePID,0);
+            return 0;
+        }
+        if(activeAlign){//alignment in progress code
+            if(firstRun){
+                robotAngle = Math.atan((Lfof)/Lsep);
+                aimAngle = Math.atan((((Usof)*Math.cos(robotAngle))-Gdis)/(((Lfof+Lfof)/2)*Math.cos(robotAngle)));
+                alignedAngle = false;
+                alignedDistance = false;
+                firstRun = false;
+            }
+            if(!alignedAngle){
+                robotAngle = Math.atan((Lfof)/Lsep);
+                turnPID = ((turnpTerm*(aimAngle-robotAngle))*(turniTerm*acum)*(turndTerm*Math.pow((aimAngle-robotAngle), 2)));
+                drivetrain.arcade(0,turnPID);
+                if(aimAngle-robotAngle > -1 && aimAngle-robotAngle < 1){
+                    alignedAngle = true;
+                }
+                return 1;
+            }
+            else if(!alignedDistance){
+                return 1;
+            }
+            else{
+                activeAlign =  false;
+                return 2;
             }
         }
         else{
-
+            return 0;
         }
     }
 }
